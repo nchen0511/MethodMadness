46370933eb1f123ace20f80c459098dc9d2f8a77
BT_1N3_30 <nchen0511@bths.edu>
2018-09-18 10:17:17 -0400

    9/18 second period commit

--- src/NChen/NChenLib.java
+++ src/NChen/NChenLib.java
@@ -27,4 +27,60 @@ public class NChenLib {
         //looks for the index of the subStr, cuts out the character at the index, and concatenates the rest of the mainStr
         return mainStr.substring(0,mainStr.indexOf(subStr)) + mainStr.substring(mainStr.indexOf(subStr)+1);
     }
+
+    //Math method 1
+    public static boolean isFibonnaci(int num){
+        //(not sure if 0 counts, so included it)
+        if(num==0){
+            return true;
+        }
+
+        //adds i by itself each time until i reaches num or goes above it,  then returning true or false, respectively
+        int i = 1;
+        while(i<num){
+            if(i==num){
+                return true;
+            }
+            i+=i;
+        }
+        return false;
+    }
+
+    //Math method 2
+    public static int sumUpTo(int num){
+        int sum = 0;
+
+        //adds sum by i, then increments i, repeats until i reaches num;
+        for(int i=0;i<=num;i++){
+            sum+=i;
+        }
+        return sum;
+    }
+
+    //Additional method
+    public static String dateStr(String date){
+        //hard coded since we know the input format is always going to be dd-mm-yyyy, replaces "/" with "-"
+        String newDate = new String("");
+        for(int i=0;i<date.length();i++){
+            if(date.substring(i,i+1).equals("/")){
+                newDate += "-";
+            } else {
+                newDate += date.substring(i,i+1);
+            }
+        }
+        return newDate;
+    }
+
+    //Challenge Method 1
+    public static String vigCipher(String msg, String key){
+        //turns everything into upper case to avoid errors
+        String msgU = msg.toUpperCase();
+        String keyU = key.toUpperCase();
+       // create an array ascii = [];
+        for(int i=0;i<key.length();i++){
+            int ascii = (int)key.charAt(i);
+            //figure out how to turn STRING into ASCII
+        }
+        //        return Character.toString((char)65);
+    }
 }
--- src/NChen/TestFile.java
+++ src/NChen/TestFile.java
@@ -1,22 +1,43 @@
 package NChen;
 
-import static NChen.NChenLib.cutOut;
-import static NChen.NChenLib.isPalindrome;
+import static NChen.NChenLib.*;
 
 public class TestFile {
 
     public static void main(String[] args){
 
-        System.out.println("String Method 1 Returns:");
+        System.out.println("String Method 1 Returns (isPalindrome):");
         //testing string method 1
         System.out.println(isPalindrome("racecar"));
         System.out.println(isPalindrome("reviver"));
         System.out.println(isPalindrome("java"));
 
-        System.out.println("\n\nString Method 2 Returns:");
+        System.out.println("\n\nString Method 2 Returns (cutOut):");
         //testing string method 2
         System.out.println(cutOut("I ate an apple.", "apple"));
         System.out.println(cutOut("Hello","He"));
         System.out.println(cutOut("Java is cool","Javascript"));
+
+        System.out.println("\n\nMath Method 1 Returns (isFibonnaci):");
+        //testing math method 1
+        System.out.println(isFibonnaci(123));
+        System.out.println(isFibonnaci(0));
+        System.out.println(isFibonnaci(144));
+
+        System.out.println("\n\nMath Method 2 Returns (sumUpTo):");
+        //testing math method 2
+        System.out.println(sumUpTo(6));
+        System.out.println(sumUpTo(0));
+        System.out.println(sumUpTo(10));
+
+        System.out.println("\n\nAdditional Method Returns (dateStr):");
+        //testing additional method
+        System.out.println(dateStr("01/01/2001"));
+        System.out.println(dateStr("01/02/3456"));
+        System.out.println(dateStr("09/18/2018"));
+
+        System.out.println("\n\nChallenge Method 1 Returns (vigCipher):");
+        //testing challenge method 1
+        System.out.println(vigCipher("test","test"));
     }
 }
